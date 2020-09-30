5ca2306729d1c82f0ea8d1acff7e6057be413636
Weerachai <weerachai.ch.61@ubu.ac.th>
2020-09-30 10:08:56 +0700

    Updates

---
+++ precache-manifest.5088df941b51e6300a52509c03ed19b0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ccfb94f5419def80a4c4351ccc6b2bf1",
+    "url": "/commathproject/index.html"
+  },
+  {
+    "revision": "af5886cbc532424cf55e",
+    "url": "/commathproject/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "0f4d8fb2336924b69ab2",
+    "url": "/commathproject/static/js/2.cf329639.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/commathproject/static/js/2.cf329639.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af5886cbc532424cf55e",
+    "url": "/commathproject/static/js/main.d6bc0e88.chunk.js"
+  },
+  {
+    "revision": "6da544f8d5c4df14080a",
+    "url": "/commathproject/static/js/runtime-main.c96b9898.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/commathproject/static/media/logo.5d5d9eef.svg"
+  }
+]);
